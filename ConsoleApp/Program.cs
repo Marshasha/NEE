4b359b71d3bcb3186204c6ae9377d8aab61b1fec
Marshasha <mspieldenner@gmx.de>
2022-01-07 15:22:15 +0100

    last version

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Net.Http.Headers;
+using BLL;
+using DTO;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
